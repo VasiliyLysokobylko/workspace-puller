50ab0b617ec4ff53d14a45ac135e608b82162d17
VasiliyLysokobylko <vasily@onpositive.com>
2019-08-26 19:42:57 +0700

    fix home page url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Workspace build tool.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/vasil7444/workspace-puller",
+    url="https://github.com/VasiliyLysokobylko/workspace-puller",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
